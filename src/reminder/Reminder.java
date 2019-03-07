11643bf1a1716fe9011e17577597a0274c46b892
Prasad <psdudeman39@gmail.com>
2019-03-07 19:08:20 +0530

    Removed the garbage code.

--- src/reminder/Reminder.java
+++ src/reminder/Reminder.java
@@ -111,88 +111,6 @@ public class Reminder implements Runnable, Serializable{
 		else Resources.show("File exists");
 	}
 	
-//	
-//	@Override
-//	public void run(){
-//		Resources.show("Inside reminder's run method");
-//		try{
-//			//	r.show("Inside run method. Features are yet to be implemented. Hit enter, I'll proceed");
-//			//	r.getch();
-//		//	Entry point of the thread
-//			//	Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
-//			File f = new File(Resources.REMFILE);
-//			LocalDateTime t;
-//			long cnt = 0;
-//			try(InputStream fis1 = new FileInputStream(f)){
-//				ObjectInputStream ois1 = new ObjectInputStream(fis1);
-//				rlist = (ReminderQueue)ois1.readObject();
-//				a = new InputReminder[rlist.queue.size()];
-//				rlist.queue.toArray(a);
-//				while(true){
-//				//	The checking code will come here
-//			/*
-//				Now will have to insert a syncronized block to access the reminderFile
-//			*/
-//					synchronized(f){
-//						try(InputStream fis = new FileInputStream(f)){
-//							ObjectInputStream ois = new ObjectInputStream(fis);
-//							rlist = (ReminderQueue)ois.readObject();
-//							a = new InputReminder[rlist.queue.size()];
-//							rlist.queue.toArray(a);
-//						}
-//					}
-//					//	showList();
-//					//	Resources.show(Thread.currentThread().getName() + ": Thread running " + cnt); 
-//					cnt++;
-//					if(a.length > 0){
-//						t = LocalDateTime.now();
-//						date = t.toLocalDate();
-//						hr = t.getHour(); min = t.getMinute(); sec = t.getSecond();
-//						//	Resources.show("Outside comparison");
-//						if((a[0].adate.equals(date) || a[0].adate.compareTo(date)<=0) && (a[0].ahour<=hr && a[0].amin<=min && a[0].asec<=sec)){
-//							Resources.show("It should pop up");
-//						//	Now remove the first element from the queue. We update the queue. Not a making a function call overe here
-//							rlist.queue.remove(0);	//	Write this queue into that file
-//							a = new InputReminder[rlist.queue.size()];
-//							rlist.queue.toArray(a);
-//							showList();
-//							synchronized(f){
-//								try(OutputStream fos = new FileOutputStream(f)){
-//									ObjectOutputStream oos = new ObjectOutputStream(fos);
-//									oos.writeObject(rlist);
-//									Resources.show("Andar hoon main");
-//									Resources.show("Hit enter now");
-//									Resources.getch();
-//								}catch(Exception e){
-//									Resources.show("line 164, Exception ka chakkar hain");
-//									Resources.showerr(e);
-//								}
-//							//	if(a.length == 0) break;
-//							}
-//						}
-//						synchronized(this){
-//							while(this.flag){
-//								wait();
-//							}//	Resources.show("Out of while");
-//						}
-//					//	Resources.show("Out sync block");
-//					}
-//				}
-//			}
-//			catch(ClassNotFoundException cnfe){
-//				Resources.show("Exception caught at line 180");
-//				Resources.showerr(cnfe);
-//			}
-//			catch(InterruptedException ie){
-//				Resources.show("linr 183, InterruptedException iis cought for the sync block of run");
-//				Resources.showerr(ie);
-//			}
-//			r.show("Bye!!!");
-//		}catch(IOException ioe){
-//			r.show("line 188, IOException caught in reminder's run methodd");
-//			r.showerr(ioe);
-//		}
-//	}
 	
 	public void showList(){
 		if(a.length > 0)
@@ -241,32 +159,7 @@ public class Reminder implements Runnable, Serializable{
 							t = LocalDateTime.now();
 							date = t.toLocalDate();
 							hr = t.getHour(); min = t.getMinute(); sec = t.getSecond();
-							//	Resources.show("Outside comparison");
-//							if((a[0].adate.equals(date) || a[0].adate.compareTo(date)<=0) && (a[0].ahour<=hr && a[0].amin<=min && a[0].asec<=sec)){
-//								Resources.show("It should pop up");
-//								Resources.show("Hit Enter to stop");
-//							//	Now start the Audio of your alarm
-//								//	new Inp	//	utReminder().input(this);	//	Can't do this....
-//								//	new Sound().playSound();	//	getch() andar hai
-//								SwingSound ss = new SwingSound();
-//								ss.setVisible(true); ss.playSound(a[0]);
-//							//	Now remove the first element from the queue. We update the queue. Not a making a function call overe here
-//								rlist.queue.remove(0);	//	Write this queue into that file
-//								a = new InputReminder[rlist.queue.size()];
-//								rlist.queue.toArray(a);
-//								showList();
-//								
-//								try(OutputStream fos = new FileOutputStream(f)){
-//									ObjectOutputStream oos = new ObjectOutputStream(fos);
-//									oos.writeObject(rlist);
-//									Resources.show("Time match ho gaya!!!");
-//									//	Resources.getch();
-//								}catch(Exception e){
-//									Resources.show("line 164, Exception ka chakkar hain");
-//									Resources.showerr(e);
-//								}
-//								//	if(a.length == 0) break;
-//							}
+						
 							if(a[0].adate.compareTo(date) < 0){
 								ss.setVisible(true); ss.playSound(a[0]); flag = true;
 							}
@@ -320,34 +213,9 @@ public class Reminder implements Runnable, Serializable{
 						Resources.showerr(ie);
 					}
 				}
-			/*
-				catch(ClassNotFoundException cnfe){
-					Resources.show("Exception caught at line 180");
-					Resources.showerr(cnfe);
-				}
-				catch(InterruptedException ie){
-					Resources.show("linr 183, InterruptedException iis cought for the sync block of run");
-					Resources.showerr(ie);
-				}
-				r.show("Bye!!!");
-			*/
-				}
-			/*
-			}
 			
-			catch(IOException ioe){
-				Resources.show("line 297, IOException caught");
-				Resources.showerr(ioe);
-			}
-			catch(ClassNotFoundException cnfe){
-				Resources.show("Exception caught at line 180");
-				Resources.showerr(cnfe);
-			}
-			//	catch(InterruptedException ie){
-				//	Resources.show("linr 183, InterruptedException iis cought for the sync block of run");
-				//	Resources.showerr(ie);
-			//	}
-			*/
+				}
+			
 		}
 		catch(Exception e){
 			Resources.show("line 310, Exception");
